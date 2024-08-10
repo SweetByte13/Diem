fb086fccea468a5180e74e18590a7849bc9d797f
Tova Yocheved Hillman <hillman.t.y@gmail.com>
2024-08-10 10:51:17 -0400

    added route for check_session

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from flask import request, session, make_response, jsonify
 from flask_restful import Resource
 from config import app, db, api
 from controllers.home import Home
-from controllers.user import Users
+from controllers.user import Users, CheckSession
 
 api.add_resource(Home, '/')
 api.add_resource(Users, '/users')
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
